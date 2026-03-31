be410998be3b933dedb56ec0b5b8fcb616269f37
Jose Rodriguez <jerodriguez2804@gmail.com>
2026-03-30 21:24:08 -0500

    image modification

--- jrodriguez-lab4.js
+++ jrodriguez-lab4.js
@@ -9,5 +9,10 @@
 
     const heroParagraph = document.querySelector("section p");
     heroParagraph.innerHTML = 'Utilize cutting-edge strategies from <strong><em>Stellar Marketing</em></strong> to help your business thrive and excel. ';
+
+    const heroSection = document.querySelector("section");
+    heroSection.style.backgroundImage = "url('https://picsum.photos/id/683/1280/720')";
+    heroSection.style.backgroundSize = "cover";
+    heroSection.style.backgroundPosition = "center";
     
 })();
